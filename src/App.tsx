7580324619c224220e8992b923df516a54fd16bf
Deepa Varma <rdvarmaa@gmail.coom>
2025-06-15 10:58:48 +0530

    Accepts a children prop of type ReactNode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import ListGroup from "./components/ListGroup";
 import ListGroupWithHook from "./components/ListGroupWithHook";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
       {/* This is where the ListGroup component is rendered */}
+
+      <Alert>
+        This is a custom alert message that can be passed as a prop.
+      </Alert>
       <ListGroup
         items={[
           "Kerala",
@@ -18,24 +23,11 @@ function App() {
         title="States in India"
         onSelectItem={(item: string) => console.log("Selected item:", item)}
       />
-
-      <ListGroup
-        items={[
-          "Balochistan",
-          "Punjab",
-          "Sindh",
-          "Lahore",
-          "Rawalpindi",
-          "Islamabad",
-        ]}
-        title="States in Pakistan"
-        onSelectItem={(item: string) => console.log("Selected state:", item)}
-      />
-
       <ListGroupWithHook />
       {/* This is where the Message component is rendered */}
     </div>
   );
 }
 export default App;
+
 // This code defines a React component named App that renders a div with the class "App" and includes the Message component.
